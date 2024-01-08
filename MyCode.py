a2a99fe68865c29f630d6ca983ada34559c5cb6b
unknown <angelluis1507hotmail.com@gmail.com>
2024-01-08 14:42:37 -0500

    20240104 my first commit

---
+++ MyCode.py
@@ -0,0 +1,12 @@
+def main():
+    name = "Angel Castro"
+    print("Good Day, Angel We want to knowif you like Programming!")
+    print()
+    print("Do you like programming"+ name +"?")
+    answer = input()
+    print("Great! Angel You said " + answer + "?")
+    
+    print("Lets Start learning some Python today")
+
+if __name__ == "__main__":
+    main()
---
+++ MyCode1.py
@@ -0,0 +1,12 @@
+def main():
+    name = input("What is your name?")
+    print(name)
+    print("Good Day, Angel We want to know if you like Programming!")
+    print()
+    answer = input("Do you like programming"+ name +"?")
+    print("Great! Angel You said " + answer + "?")
+
+    print("Lets Start learning some Python today")
+
+if __name__ == "__main__":
+    main()

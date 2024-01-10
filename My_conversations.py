fa93071b15427378198c8a5fb595315334cd93f7
unknown <angelluis1507hotmail.com@gmail.com>
2024-01-10 13:29:20 -0500

    20240110 H+S class

---
+++ My_conversations.py
@@ -0,0 +1,16 @@
+def conversation():
+    print("Hello Angel, Do you like Coding in Python? Yes or No")
+    answer = input()
+    if answer == "Yes":
+        print("That's good - Let's get started on Python!")
+    else:
+        print("We hope to see you soon Angel! ")
+    print("Goodbye")
+
+def main():
+    print("Welcome to Coversations with Python!")
+    conversation()
+    print("Thanks for Chatting with Python")
+
+if __name__ == "__main__":
+    main()
---
+++ Thawne.py
@@ -0,0 +1,9 @@
+def main():
+    name = input("What is your name?")
+    print("Good Day, Angel We want to know if you like Programming!")
+    answer = input("Do you like programming "+name+ "?")
+    print('Great! Angel You said, "' +answer+ "!")
+    print("Let's learn some Python today Angel")
+
+if __name__ == "__main__":
+    main()

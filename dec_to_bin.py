906d3742b2e42c4ec07bb6ed4ff2f0ce6a8e2786
unknown <angelluis1507hotmail.com@gmail.com>
2024-01-31 13:40:44 -0500

    20240131 H&S class work

--- dec_to_bin.py
+++ dec_to_bin.py
@@ -7,9 +7,15 @@ def binary_to_decimal(number):
         result = str(remainder) + result  # Convert remainder to string before concatenating
     return result
 
+def is_binary(number):
+    return all(bit in '01' for bit in number)  # Check if all characters are '0' or '1'
+
 def main():
     num = input("Enter Binary Number:")
-    print("Number entered: {}, Binary: {}".format(num, binary_to_decimal(num)))
+    if is_binary(num):
+        print("Decimal: {}, Binary: {}".format(binary_to_decimal(num), num))
+    else:
+        print("Invalid input. Please enter a binary number consisting of '0' and '1'.")
 
 if __name__ == "__main__":
     main()
